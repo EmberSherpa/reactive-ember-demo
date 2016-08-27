f4a33b9e74d7cecc08b3137d7e3398cba5b8603a
Taras Mankovski <tarasm@gmail.com>
2016-08-27 13:14:32 +0530

    Removed code from issues component

--- app/components/github-repo/issues.js
+++ app/components/github-repo/issues.js
@@ -3,77 +3,22 @@ import { task } from 'ember-concurrency';
 
 const {
   computed,
-  inject: { service },
-  assign
+  inject: { service }
 } = Ember;
 
 export default Ember.Component.extend({
   ajax: service(),
-  
-  init() {
-    this._super(...arguments);
-
-    this.setProperties({
-      issues: [],
-      selected: []
-    });
-
-    this.get('fetchIssues').perform();
-  },
   
   url: computed('repo', function(){
     let repo = this.get('repo');
     return `https://api.github.com/repos/${repo}/issues`;
   }),
 
-  fetchIssues: task(function *() {
+  fetchIssues: task(function *(page) {
     let url = this.get('url');
-    let result = yield this.get('ajax').request(url);
+
+    let result = yield this.get('ajax').request(`${url}?page=${page}`);
 
     this.set('issues', result);
-  }),
-
-  filteredIssuesByPullRequest: computed('issues.[]', 'showPullRequests', function(){
-    let issues = this.get('issues');
-    let showPullRequests = this.get('showPullRequests');
-
-    if (showPullRequests) {
-      return issues;
-    }    
-
-    return issues.filter( ({ pull_request }) => !pull_request );
-  }),
-
-  issuesWithSelection: computed('filteredIssuesByPullRequest.[]', 'selected.[]', function(){
-    let issues = this.get('filteredIssuesByPullRequest');
-    let selected = this.get('selected');
-
-    return issues.map((issue) => {
-      return assign({}, issue, {
-        original: issue,
-        isSelected: selected.includes(issue)
-      });
-    });
-  }),
-
-  actions: {
-    toggleSelection(issue) {
-      let selected = this.get('selected');
-
-      if (selected.includes(issue)) {
-        selected.removeObject(issue);
-      } else {
-        selected.pushObject(issue);
-      }
-    },
-    toggleShowSelection() {
-      this.setProperties({
-        selected: [],
-        allowSelection: !this.get('allowSelection')
-      });
-    },
-    toggleShowPullRequests() {
-      this.toggleProperty('showPullRequests');
-    }
-  }
+  })
 });
